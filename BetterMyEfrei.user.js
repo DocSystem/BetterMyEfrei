f9dc0d9df963bac9e8a792b751d9b86b016aca01
Doryan Denis <doryan.denis@efrei.net>
2026-01-20 10:37:52 +0100

    fix: nettoyer les commentaires inutiles dans le script utilisateur

--- BetterMyEfrei.user.js
+++ BetterMyEfrei.user.js
@@ -6,16 +6,16 @@
 // @author       DocSystem & Doryan D. & Mathu_lmn & Mat15
 // @match        https://www.myefrei.fr/portal/student/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=www.myefrei.fr
-// @match        https://www.myefrei.fr/portal/student/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=www.myefrei.fr
+
+
 // @updateURL    https://github.com/DocSystem/BetterMyEfrei/raw/refs/heads/main/BetterMyEfrei.user.js
 // @downloadURL  https://github.com/DocSystem/BetterMyEfrei/raw/refs/heads/main/BetterMyEfrei.user.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js
 // @resource     PDF_WORKER https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js
 // @grant        GM_getResourceText
 // @grant        GM_getResourceURL
-// @grant        GM_getResourceText
-// @grant        GM_getResourceURL
+
+
 // @grant        GM_xmlhttpRequest
 // @grant        unsafeWindow
 // @connect      cdnjs.cloudflare.com
